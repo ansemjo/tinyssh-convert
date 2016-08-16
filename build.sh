82dc22e780089e416c95da6624aaef76db2f5177
Anton Semjonov <anton@semjonov.de>
2016-08-16 17:21:49 +0200

    script should still fail when comilation fails ..

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ name=$1
 
 err() { >&2 printf '\e[1mERR!\e[0m  %s\n' "$*"; exit 1; }
 
-gcc -g -o "${name}" *.c
+gcc -g -o "${name}" *.c || err "compilation failed!"
 
 printf '\e[1m%s\e[0m %s%q\n' 'SUCCESS!' 'Compiled file is saved in: ' "./${name}"

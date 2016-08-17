d37b0111e3980f557e3c350b0eac145e2304a0db
Anton Semjonov <anton@semjonov.de>
2016-08-17 23:23:34 +0200

    reformulate prompt to remove traces of 'filenames'

--- utils.c
+++ utils.c
@@ -13,30 +13,30 @@ int strnzero (const char *str) {
 }
 
 
-/* ask for a filename */
-void prompt (const char *prmt, char *fn, size_t fn_len, const char *dfn) {
-    char buf[fn_len];
+/* ask for user input */
+void prompt (const char *prmt, char *dest, size_t dest_len, const char *df_value) {
+    char buf[dest_len];
     
-    /* if default path is given, copy that to fn already */
-    if (strnzero(dfn))
-        snprintf(fn, fn_len, "%s", dfn);
+    /* if default value is given, copy that to dest already */
+    if (strnzero(df_value))
+        snprintf(dest, dest_len, "%s", df_value);
 
     /* display prompt */
-    if (strnzero(fn))
-	    printf("%s [%s]: ", prmt, fn);
+    if (strnzero(dest))
+	    printf("%s [%s]: ", prmt, dest);
     else
-        printf("%s: ", fn);
+        printf("%s: ", dest);
 	fflush(stdout);
 	
-    /* get filename from stdin */
+    /* get input from stdin */
     if (fgets(buf, sizeof buf, stdin) == NULL)
-        exit(1); /* is this what we want? */
+        exit(1); /* TODO is this what we want? */
     
     /* find newline and replace by \0 */
 	buf[strcspn(buf, "\n")] = '\0';
 
-    /* if the buffer now has nonzero length, copy that to filename */
-	if (strnzero(buf)) strncpy(fn, buf, fn_len);
+    /* if the buffer now has nonzero length, copy that to dest */
+	if (strnzero(buf)) strncpy(dest, buf, dest_len);
 }
 
 
